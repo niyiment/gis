475f315553f7a9834d6672bcd70cdd8fb2f5c616
Niyi Ogungbemi <niyiment@gmail.com>
2022-07-16 03:09:05 +0100

    initial commit

---
+++ src/test/java/org/nomisng/gis/GisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nomisng.gis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
